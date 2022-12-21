d0ba7f8017fe98c6788a05d844e1f6306f64fdbe
Pilevich Sasha <pilevichsasha@gmail.com>
2022-12-21 13:05:21 +0300

    fix:change level

---
+++ array.js
@@ -0,0 +1,95 @@
+export const cardsForLevelOne = [
+  {
+    id: 1,
+    name: "bird",
+    img: "./assets/bird.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 2,
+    name: "cat",
+    img: "./assets/cat.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 3,
+    name: "elephant",
+    img: "./assets/elephant.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 4,
+    name: "kangary",
+    img: "./assets/kangary.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 5,
+    name: "koala",
+    img: "./assets/koala.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 6,
+    name: "labrador",
+    img: "./assets/labrador.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 7,
+    name: "lion",
+    img: "./assets/lion.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 8,
+    name: "monkey",
+    img: "./assets/monkey.jpg",
+    imgBack: "./assets/front.svg",
+  },
+];
+
+export const cardsForLevelTwo = [
+  ...cardsForLevelOne,
+  {
+    id: 9,
+    name: "pandaastro",
+    img: "./assets/pandaastro.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 10,
+    name: "pandacarate",
+    img: "./assets/pandacarate.jpg",
+    imgBack: "./assets/front.svg",
+  },
+];
+
+export const cardsForLevelThree = [
+  ...cardsForLevelTwo,
+  {
+    id: 11,
+    name: "pingui",
+    img: "./assets/pingui.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 12,
+    name: "rabbit",
+    img: "./assets/rabbit.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 13,
+    name: "yoga",
+    img: "./assets/yoga.jpg",
+    imgBack: "./assets/front.svg",
+  },
+  {
+    id: 14,
+    name: "zebra",
+    img: "./assets/zebra.jpg",
+    imgBack: "./assets/front.svg",
+  },
+
+]
--- script.js
+++ script.js
@@ -1,24 +1,68 @@
+import {
+  cardsForLevelOne,
+  cardsForLevelTwo,
+  cardsForLevelThree,
+} from "./array.js";
 const root = document.getElementById("#root");
 const cards = document.querySelectorAll(".card-wrapper");
-let count = document.querySelector('.count')
-let title = document.querySelector('.title-level')
-let levelOne = document.querySelector('.level-one')
-let levelTwo = document.querySelector('.level-two')
-let levelThree = document.querySelector('.level-three')
-let winner = document.querySelector('.winner')
+let count = document.querySelector(".count");
+let title = document.querySelector(".title-level");
+let levelOne = document.querySelector(".level-one");
+let levelTwo = document.querySelector(".level-two");
+let levelThree = document.querySelector(".level-three");
+let winner = document.querySelector(".winner");
+let cardWrapper;
+let cloneCardsLevelOne = [...cardsForLevelOne];
+let cardsLevelOne = cloneCardsLevelOne.concat(cardsForLevelOne);
+let cloneCardsLevelTwo = [...cardsForLevelTwo];
+let cardsLevelTwo = cloneCardsLevelTwo.concat(cardsForLevelTwo);
+let cloneCardsLevelThree = [...cardsForLevelThree];
+let cardsLevelThree = cloneCardsLevelThree.concat(cardsForLevelThree);
+
+createGame(cardsLevelOne);
+function createGame(array) {
+  array.forEach((item) => {
+    cardWrapper = document.createElement("div");
+    cardWrapper.className = "card-wrapper";
+    cardWrapper.setAttribute("data-image", item.name);
+    const back = document.createElement("img");
+    back.className = "back";
+    back.src = item.img;
+    const front = document.createElement("img");
+    front.className = "front";
+    front.src = item.imgBack;
+    cardWrapper.append(back, front);
+    cardWrapper.addEventListener("click", flipCard);
+    if (array.length === 16) {
+      levelOne.append(cardWrapper);
+      title.innerHTML = "Level : 1";
+    } else if (array.length === 20) {
+      levelTwo.append(cardWrapper);
+      levelOne.style.display = "none";
+      title.innerHTML = "Level : 2";
+    } else if (array.length === 28) {
+      levelThree.append(cardWrapper);
+      levelOne.style.display = "none";
+      levelTwo.style.display = "none";
+      title.innerHTML = "Level : 3";
+    }
+    let random = Math.floor(Math.random() * 28);
+    cardWrapper.style.order = random;
+  });
+  console.log(array.length);
+}
 
 let hasFlippedCard = false;
 let firstCard;
 let secondCard;
 let lockBoard = false;
 let score = 0;
-
+let interval;
 let isLevelOne = true;
 let isLevelTwo = false;
 let isLevelThree = false;
 
 function flipCard() {
-  
   if (lockBoard) return;
   if (this === firstCard) return;
   this.classList.add("flip");
@@ -26,6 +70,7 @@ function flipCard() {
     //first click
     hasFlippedCard = true;
     firstCard = this;
+
     return;
   } else {
     //second click
@@ -39,13 +84,11 @@ function flipCard() {
       firstCard.removeEventListener("click", flipCard);
       secondCard.removeEventListener("click", flipCard);
       resetBoard();
-      if(isLevelOne){
-        checkWinOne()
+      if (isLevelOne) {
+        checkWinOne();
       } else {
-        checkWinTwo()
+        checkWinTwo();
       }
-      
-      
     } else {
       score += 1;
       count.innerHTML = `Count: ${score}`;
@@ -58,64 +101,61 @@ function flipCard() {
       }, 1500);
     }
   }
-  
-  
 }
 
-function checkWinOne () {
-  
-  let one = []
-  cards.forEach((item)=>{
-    if(item.className === 'card-wrapper flip'){
-      one.push(item)
-    } 
-    if(one.length === 16){
-      levelOne.style.display = 'none'
-      winner.style.display = 'block'
-      setTimeout(() => {
-        levelTwo.style.display = 'grid'
-        isLevelTwo = true
-        isLevelOne = false
-        
-      }, 2000);
-    }else {
-      return
-    }
-  })
-  console.log(one.length)
-}
-function checkWinTwo () {
-  let two = []
-  cards.forEach((item)=>{
-    if(item.className === 'card-wrapper flip'){
-      two.push(item)
-    } 
-    if(two.length === 36){
-      levelTwo.style.display = 'none'
-      winner.style.display = 'block'
-      setTimeout(() => {
-        levelThree.style.display = 'grid'
-        isLevelTwo = false
-      }, 2000);
-    }
-    
-  })
-  console.log(two.length)
-}
-
-(function randomPosition() {
+function checkWinOne() {
+  let cards = document.querySelectorAll(".card-wrapper");
+  let one = [];
   cards.forEach((item) => {
-    let random = Math.floor(Math.random() * 28);
-    item.style.order = random;
+    if (item.className === "card-wrapper flip") {
+      one.push(item);
+    }
+    if (one.length === 16) {
+      setTimeout(() => {
+        levelOne.style.display = "none";
+        winner.style.display = "block";
+        isLevelTwo = true;
+        isLevelOne = false;
+      }, 1000);
+      setTimeout(() => {
+        createGame(cardsLevelTwo);
+        isLevelTwo = true;
+        isLevelOne = false;
+        winner.style.display = "none";
+        count.innerHTML = `Count: 0`;
+      }, 5000);
+    } else {
+      return;
+    }
   });
-})();
+}
+function checkWinTwo() {
+  let two = [];
+  let cards = document.querySelectorAll(".card-wrapper");
+  cards.forEach((item) => {
+    if (item.className === "card-wrapper flip") {
+      two.push(item);
+    }
+    if (two.length === 36) {
+      winner.style.display = "block";
+      setTimeout(() => {
+        levelTwo.style.display = "none";
+        createGame(cardsLevelThree);
+        isLevelTwo = false;
+        winner.style.display = "none";
+      }, 2000);
+    } else {
+      return;
+    }
+  });
+}
+
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false][(firstCard, secondCard)] = [
     null,
     null,
   ];
-  
 }
-cards.forEach((item) => {
-  item.addEventListener("click", flipCard);
-});
+
+
+
